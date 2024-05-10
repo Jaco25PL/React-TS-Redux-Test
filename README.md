b895c58f6bec32a286d96a503b0e2e93ac786850
PiedraL <joaquinlangone12@gmail.com>
2024-05-09 23:18:59 -0300

    Displaing mock data to create the table

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # TEST
 
 **The objective of this technical test is to create a simitar application to the one provided in this link:
-https ://xxxxx. To achieve this, you must use the API provided by https ://randomuser.me/**
+https ://xxxxx. To achieve this, you must use the API provided (HERE)[https ://randomuser.me/]**
 
 ## Here are the steps to follow
-- Fetch 100 rows of data using the API.
-- Display the data in a table format, similar to the example.
-- Provide the option to color rows as shown in the example.
-- Attow the data to be sorted by country as demonstrated in the example.
-- Enable the ability to delete a row as shown in the example.
-- Implement a feature that allows the user to restore the initial state, meaning that all deleted rows will be recovered.
-- Handle any potential errors that may occur.
-- Implement a feature that allows the user to fitter the data by country.
-- Avoid sorting users again the data when the user is changing fitter by country.
-- Sort by clicking on the column header.
-- Provide a README.md file with instructions on how to run the application.
+-[]- Fetch 100 rows of data using the API.
+-[]- Display the data in a table format, similar to the example.
+-[]- Provide the option to color rows as shown in the example.
+-[]- Attow the data to be sorted by country as demonstrated in the example.
+-[]- Enable the ability to delete a row as shown in the example.
+-[]- Implement a feature that allows the user to restore the initial state, meaning that all deleted rows will be recovered.
+-[]- Handle any potential errors that may occur.
+-[]- Implement a feature that allows the user to fitter the data by country.
+-[]- Avoid sorting users again the data when the user is changing fitter by country.
+-[]- Sort by clicking on the column header.
+-[]- Provide a README.md file with instructions on how to run the application.
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import results from './mock/results.json'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App () {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const users = results.results
+
+    
+
+    return(
+        <main>
+            <h1 className='text-4xl'>Users</h1>
+
+            <table className="w-full text-sm text-left rtl:text-right ">
+                <thead className=" text-xs  uppercase ">
+                    <tr className="flex justify-between [&_*]:text-base">
+                        <th scope="col" className="w-[100px] px-6 py-3">
+                            Profile
+                        </th>
+                        <th scope="col" className="w-[250px] px-6 py-3">
+                            NAME
+                        </th>
+                        <th scope="col" className="w-[250px] px-3 py-3">
+                            LAST NAME
+                        </th>
+                        <th scope="col" className="flex  px-6 py-3">
+                            COUNTRY
+                        </th>
+                        <th scope="col" className="flex  px-6 py-3">
+                            ACTION
+                        </th>
+                    </tr>
+                </thead>
+            </table>
+
+            <table>
+                <tbody>
+                    {
+                        users?.map((user, index) => (
+                            <tr key={index} className='h-full border-solid border-gray-500 border-b-2 py-4 font-medium whitespace-nowrap flex justify-between items-center'>
+
+                                <th className='w-[100px] px-6 py-4 font-bold  whitespace-nowrap'>
+                                    <img src={user.picture.thumbnail} alt={`${user.name.first} ${user.name.last}`} />
+                                </th>
+
+                                <th className='w-[250px] px-6 py-4 flex items-center'>
+                                    <span>{user.name.first}</span>
+                                </th>
+                                <th className="w-[250px] pr-6 py-4">
+                                    <span>{user.name.last}</span>
+                                </th>
+                                <th className='px-6 py-4 flex items-center gap-2'>
+                                    <span>{user.location.country}</span>
+                                </th>
+                                <th className='px-6 py-4 flex items-center gap-2'>
+                                    <button type='button'>delete</button>
+                                </th>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            
+
+        </main>
+    )
+
+
+
+
 }
-
-export default App
